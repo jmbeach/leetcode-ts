47c7c8f686073cbac2ce853a0f9c8f2716b1ceef
jmbeach <jaredbeachdesign@gmail.com>
2022-08-28 10:05:24 -0400

    reverse

--- linked lists/reverse.ts
+++ linked lists/reverse.ts
@@ -8,24 +8,56 @@ class ListNode {
 }
 
 function reverseList(head: ListNode | null): ListNode | null {
-  const nodes = [];
-  let next = head;
-  if (head == null) return null;
-  while (next !== null) {
-    nodes.push(next);
-    next = next.next;
+  if (head === null) return null;
+  let current = head;
+  let prev = null;
+  while (current) {
+    const savedNext = current.next
+    current.next = prev;
+    prev = current;
+    current = savedNext
   }
-  const result: ListNode = {
-    next: null,
-    val: nodes[nodes.length - 1].val
-  };
-  next = result;
-  for (let i = nodes.length - 2; i >= 0; i--) {
-    next.next = {
-      next: null,
-      val: nodes[i].val
-    }
-    next = next?.next;
-  }
-  return result;
+  return prev;
 };
+
+const tests = [
+  { data: [1,2,3,4,5], expect: [5,4,3,2,1] }
+]
+
+for (const test of tests) {
+  const headTest = new ListNode(test.data[0])
+  let next = headTest
+  for (const x of test.data.slice(1)) {
+    const newNext = new ListNode(x)
+    next.next = newNext
+    next = newNext
+  }
+
+  const headExpect = new ListNode(test.expect[0])
+  next = headExpect
+  for (const x of test.expect.slice(1)) {
+    const newNext = new ListNode(x)
+    next.next = newNext
+    next = newNext
+  }
+
+  const startTime = new Date();
+  const result = reverseList(headTest);
+  const endTime = new Date();
+  const resultAsArray = []
+  let current = result;
+  while (current) {
+    resultAsArray.push(current.val)
+    current = current.next
+  }
+
+  if (resultAsArray.toString() !== test.expect.toString()) {
+    throw new Error(`${JSON.stringify(test)} - ${resultAsArray}`)
+  }
+  const duration = endTime.getTime() - startTime.getTime();
+  if (duration > 500) {
+    throw new Error(`${JSON.stringify(test)} - took too long - ${duration}ms`)
+  }
+}
+
+console.log('success')
