58c5e148408aba48d1f5c192298e22f3b3b18ef9
jmbeach <jaredbeachdesign@gmail.com>
2022-11-08 23:30:40 -0500

    improvements

--- arrays and hashing/rhombus.ts
+++ arrays and hashing/rhombus.ts
@@ -4,6 +4,7 @@ function getBiggestThree(grid: number[][]): number[] {
   const rhombusSums: Set<number> = new Set();
   for (let rhombusSize = 1; rhombusSize <= maxRhombus; rhombusSize += 2) {
     for (let row = 0; row <= grid.length - rhombusSize; row++) {
+      const endRow = row + rhombusSize - 1;
       for (let col = Math.floor(rhombusSize / 2); col < grid[0].length; col++) {
         let sum = 0;
         for (let i = row; i <= row + Math.floor(rhombusSize / 2); i++) {
@@ -15,10 +16,9 @@ function getBiggestThree(grid: number[][]): number[] {
             sum += grid[i][j];
           }
         }
-        const endRow = row + rhombusSize - 1;
         for (let i = endRow; i > row + Math.floor(rhombusSize / 2); i--) {
           for (
-            let j = col - (i - endRow);
+            let j = col - (endRow - i);
             j <= col - (i - row) + (i - row) * 2;
             j += Math.max(1, (i - row) * 2)
           ) {
@@ -33,23 +33,23 @@ function getBiggestThree(grid: number[][]): number[] {
 }
 
 const tests = [
-  // {
-  //   data: [
-  //     [3, 4, 5, 1, 3],
-  //     [3, 3, 4, 2, 3],
-  //     [20, 30, 200, 40, 10],
-  //     [1, 5, 5, 4, 1],
-  //     [4, 3, 2, 2, 5],
-  //   ],
-  //   expect: [228, 216, 211],
-  // },
   {
     data: [
-      [20,  17, 9,  13, 5,  2,  9,  1,  5],
-      [14,  9,  9,  9,  16, 18, 3,  4,  12],
-      [18,  15, 10, 20, 19, 20, 15, 12, 11],
-      [19,  16, 19, 18, 8,  13, 15, 14, 11],
-      [4,   19, 5,  2,  19, 17, 7,  2,  2],
+      [3, 4, 5, 1, 3],
+      [3, 3, 4, 2, 3],
+      [20, 30, 200, 40, 10],
+      [1, 5, 5, 4, 1],
+      [4, 3, 2, 2, 5],
+    ],
+    expect: [228, 216, 211],
+  },
+  {
+    data: [
+      [20, 17, 9, 13, 5, 2, 9, 1, 5],
+      [14, 9, 9, 9, 16, 18, 3, 4, 12],
+      [18, 15, 10, 20, 19, 20, 15, 12, 11],
+      [19, 16, 19, 18, 8, 13, 15, 14, 11],
+      [4, 19, 5, 2, 19, 17, 7, 2, 2],
     ],
     expect: [107, 103, 102],
   },
